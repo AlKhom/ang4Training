07538c39b4f840f27230e08ca6910459810d1a08
khomenker <autohelp123@gmail.com>
2017-10-26 13:23:51 +0300

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { TryInputComponent } from './try-input/try-input.component';
+import {APP_ROUTE_PROVIDER} from "./app.routes";
+import {DirectoryComponent} from "./directory/directory.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TryInputComponent
+    TryInputComponent, DirectoryComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule, APP_ROUTE_PROVIDER
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import {DirectoryComponent} from "./directory/directory.component";
+import {HomeComponent} from "./home/home.component";
+import {TryInputComponent} from "./try-input/try-input.component";
+import {RouterModule} from "@angular/router";
+
+const APP_ROUTER = [
+  { path: 'direc', component: DirectoryComponent},
+  {path : '', component: HomeComponent},
+  {path: 'try', component: TryInputComponent}
+];
+
+export const APP_ROUTE_PROVIDER = [
+  RouterModule.forRoot(APP_ROUTER)
+]
+
